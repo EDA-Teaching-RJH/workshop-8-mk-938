0707f741cd1a44e526626fecc2e4c363f553a139
mk-938 <mk938@kent.ac.uk>
2024-11-22 10:45:59 +0000

    hi

---
+++ test_calc.py
@@ -0,0 +1,22 @@
+import pytest
+from Calculator import add, sub, div, mult
+def test_add():
+    assert add(1,1) == 2
+    assert add(2,1) == 3
+    assert add(0,0) == 0
+
+def test_sub():
+    assert sub(1,1) == 0
+    assert sub(10,3) == 7
+    assert sub(0,0) == 0
+   
+def test_div():
+    assert div(10,2) == 5
+    assert div(2,0) == 0
+    assert div(2,1) == 2
+  
+def test_mult():
+    assert mult(10,1) == 10
+    assert mult(5,0) == 0
+    assert mult(7,3) == 21
+
